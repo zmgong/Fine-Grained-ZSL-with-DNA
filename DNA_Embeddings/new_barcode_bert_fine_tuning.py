fc46a7aad07424d0c15d743c89b85a95e69f5c8a
zmgong <ming2280089874@gmail.com>
2024-10-16 02:11:59 -0700

    Change port number.

--- DNA_Embeddings/new_barcode_bert_fine_tuning.py
+++ DNA_Embeddings/new_barcode_bert_fine_tuning.py
@@ -415,7 +415,7 @@ def test(dataloader_test, device, model):
 
 def ddp_setup(rank: int, world_size: int):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '5000'
+    os.environ['MASTER_PORT'] = '8000'
     torch.distributed.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
